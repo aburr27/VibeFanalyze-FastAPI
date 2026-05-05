cc6bdc30f65dbb51aafe215b806e40fbb1c14d1b
Arebria Burr <47797027+aburr27@users.noreply.github.com>
2026-05-05 10:15:10 -0500

    Update main.py
    Adding other routes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.db.base import Base
 from app.db.session import engine
 
 # Import auth routes
-from app.api.routes import auth, teams
+from app.api.routes import auth, games, players, teams, users
 
 app = FastAPI(title="Vibe-Fanalyze API")
 
@@ -13,7 +13,10 @@ Base.metadata.create_all(bind=engine)
 
 # Register routes
 app.include_router(auth.router)
+app.include_router(games.router)
+app.include_router(players.router)
 app.include_router(teams.router)
+app.include_router(users.router)
 
 @app.get("/")
 def root():
